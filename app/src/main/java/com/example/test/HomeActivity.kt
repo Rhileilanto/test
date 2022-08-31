e1a2e5d17478cb604c2fddc39b05425b9cffdb84
Rhileilanto <leah79675@gmail.com>
2022-08-31 13:30:23 +0300

    my first android app commit

--- app/src/main/java/com/example/test/HomeActivity.kt
+++ app/src/main/java/com/example/test/HomeActivity.kt
@@ -1,11 +1,23 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //       button with intent
+
+        val btn = findViewById<TextView>(R.id.Back_intent)
+
+        btn.setOnClickListener {
+            val try_intent = Intent(this, SplashActivity::class.java )
+            startActivity(try_intent)
+        }
     }
 }
--- app/src/main/java/com/example/test/SplashActivity.kt
+++ app/src/main/java/com/example/test/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.example.test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//       button with intent
+
+         val btn = findViewById<Button>(R.id.button_intent)
+
+        btn.setOnClickListener {
+            val try_intent = Intent(this, HomeActivity::class.java )
+            startActivity(try_intent)
+        }
     }
 }
